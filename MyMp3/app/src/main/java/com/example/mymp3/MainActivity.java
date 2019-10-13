11ab4e5ce5a0bfbf7c187f8094ac711c5f191ba4
youngmu byun <tyg04308@naver.com>
2019-10-13 16:59:58 +0900

    add butterknife

--- MyMp3/app/src/main/java/com/example/mymp3/MainActivity.java
+++ MyMp3/app/src/main/java/com/example/mymp3/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.mymp3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.Title)
+    TextView title;
+    @BindView(R.id.Thumbnail)
+    ImageView thumbnail;
+    @BindVIew(R.id.Play)
+    Button play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.Play)
+    public void OnPlayClick(View view){
+        play.setBackground(R.id.Play);
     }
 }
