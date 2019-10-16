1efa10359296c8c1c9254fc72a7416d3c47a1f4b
tyg04308 <tyg04308@naver.com>
2019-10-17 03:35:33 +0900

    implement bluetooth connection

--- MyMp3/app/src/main/java/com/example/mymp3/MainActivity.java
+++ MyMp3/app/src/main/java/com/example/mymp3/MainActivity.java
@@ -1,15 +1,37 @@
 package com.example.mymp3;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.accessibility.AccessibilityManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +40,17 @@ public class MainActivity extends AppCompatActivity {
     private Button play;
     private Button next;
     private Button prev;
+
+    private static final int REQUEST_ENABLE_BT = 10; // 블루투스 활성화 상태
+    private BluetoothAdapter bluetoothAdapter; // 블루투스 어댑터
+    private Set<BluetoothDevice> devices; // 블루투스 디바이스 데이터 셋
+    private BluetoothDevice bluetoothDevice; // 블루투스 디바이스
+    private BluetoothSocket bluetoothSocket = null; // 블루투스 소켓
+    private OutputStream outputStream = null; // 블루투스에 데이터를 출력하기 위한 출력 스트림
+    private InputStream inputStream = null; // 블루투스에 데이터를 입력하기 위한 입력 스트림
+    private Thread workerThread = null; // 문자열 수신에 사용되는 쓰레드
+    private byte[] readBuffer; // 수신 된 문자열을 저장하기 위한 버퍼
+    private int readBufferPosition; // 버퍼 내 문자 저장 위치
 
     private MediaPlayer mediaPlayer;
     private int[] songID = new int[]{R.raw.nam, R.raw._2002, R.raw.speechless, R.raw.punch, R.raw.workaholic};
@@ -37,6 +70,26 @@ public class MainActivity extends AppCompatActivity {
         play = findViewById(R.id.Play);
         prev = findViewById(R.id.Prev);
         next = findViewById(R.id.Next);
+
+        checkPermission();
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter(); // 블루투스 어댑터를 디폴트 어댑터로 설정
+
+        if (bluetoothAdapter != null) { // 디바이스가 블루투스를 지원 할 때
+            if (bluetoothAdapter.isEnabled()) { // 블루투스가 활성화 상태 (기기에 블루투스가 켜져있음)
+                selectBluetoothDevice(); // 블루투스 디바이스 선택 함수 호출
+            } else { // 블루투스가 비 활성화 상태 (기기에 블루투스가 꺼져있음)
+                // 블루투스를 활성화 하기 위한 다이얼로그 출력
+                Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                // 선택한 값이 onActivityResult 함수에서 콜백된다.
+                startActivityForResult(intent, REQUEST_ENABLE_BT);
+            }
+        }
+
+        else{
+            Toast.makeText(this, "블루투스를 지원해야 합니다.", Toast.LENGTH_LONG).show();
+            finish();
+        }
 
         mediaPlayer = MediaPlayer.create(MainActivity.this, songID[songIdx]);
         mediaPlayer.start();
@@ -59,42 +112,14 @@ public class MainActivity extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (songIdx == songID.length - 1) {
-                    songIdx = 0;
-                } else {
-                    songIdx++;
-                }
-
-                if (mediaPlayer != null) {
-                    mediaPlayer.release();
-                    mediaPlayer = null;
-                }
-
-                mediaPlayer = MediaPlayer.create(MainActivity.this, songID[songIdx]);
-                mediaPlayer.start();
-                isPlay = true;
-                update();
+                nextSong();
             }
         });
 
         prev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (songIdx == 0) {
-                    songIdx = songID.length - 1;
-                } else {
-                    songIdx--;
-                }
-
-                if (mediaPlayer != null) {
-                    mediaPlayer.release();
-                    mediaPlayer = null;
-                }
-
-                mediaPlayer = MediaPlayer.create(MainActivity.this, songID[songIdx]);
-                mediaPlayer.start();
-                isPlay = true;
-                update();
+                prevSong();
             }
         });
 
@@ -114,33 +139,260 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    void checkPermission(){
+        ArrayList<String> permissions = new ArrayList<String>();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.BLUETOOTH_ADMIN);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
+            permissions.add(Manifest.permission.BLUETOOTH);
+        }
+
+        if(permissions.size() > 0) {
+            String[] reqPermissionArray = new String[permissions.size()];
+            reqPermissionArray = permissions.toArray(reqPermissionArray);
+            ActivityCompat.requestPermissions(this, reqPermissionArray, REQUEST_ENABLE_BT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (requestCode == RESULT_OK) { // '사용'을 눌렀을 때
+                selectBluetoothDevice(); // 블루투스 디바이스 선택 함수 호출
+            }
+            else{
+                Toast.makeText(this, "블루투스 권한을 허용해야 합니다.", Toast.LENGTH_LONG).show();
+                //finish();
+            }
+        }
+    }
+
+    public void selectBluetoothDevice() {
+        // 이미 페어링 되어있는 블루투스 기기를 찾습니다.
+        devices = bluetoothAdapter.getBondedDevices();
+
+        // 페어링 된 디바이스의 크기를 저장
+        int pariedDeviceCount = devices.size();
+
+        // 페어링 되어있는 장치가 없는 경우
+        if(pariedDeviceCount == 0) {
+            Toast.makeText(this, "페어링 된 장치가 없습니다.", Toast.LENGTH_LONG).show();
+        }
+
+        // 페어링 되어있는 장치가 있는 경우
+        else {
+            // 디바이스를 선택하기 위한 다이얼로그 생성
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("페어링 되어있는 블루투스 디바이스 목록");
+
+            // 페어링 된 각각의 디바이스의 이름과 주소를 저장
+            List<String> list = new ArrayList<>();
+
+            // 모든 디바이스의 이름을 리스트에 추가
+            for(BluetoothDevice bluetoothDevice : devices) {
+                list.add(bluetoothDevice.getName());
+            }
+
+            list.add("취소");
+
+            // List를 CharSequence 배열로 변경
+            final CharSequence[] charSequences = list.toArray(new CharSequence[list.size()]);
+            list.toArray(new CharSequence[list.size()]);
+
+            // 해당 아이템을 눌렀을 때 호출 되는 이벤트 리스너
+            builder.setItems(charSequences, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // 해당 디바이스와 연결하는 함수 호출
+                    connectDevice(charSequences[which].toString());
+                }
+            });
+
+            // 뒤로가기 버튼 누를 때 창이 안닫히도록 설정
+            builder.setCancelable(false);
+
+            // 다이얼로그 생성
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }
+    }
+
+    public void connectDevice(String deviceName) {
+
+        boolean isFound = false;
+        // 페어링 된 디바이스들을 모두 탐색
+        for(BluetoothDevice tempDevice : devices) {
+            // 사용자가 선택한 이름과 같은 디바이스로 설정하고 반복문 종료
+            if(deviceName.equals(tempDevice.getName())) {
+                bluetoothDevice = tempDevice;
+                isFound = true;
+                break;
+            }
+        }
+
+        if (!isFound){
+            return;
+        }
+
+        // UUID 생성
+        UUID uuid = java.util.UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+        // Rfcomm 채널을 통해 블루투스 디바이스와 통신하는 소켓 생성
+        try {
+            bluetoothSocket = bluetoothDevice.createRfcommSocketToServiceRecord(uuid);
+            bluetoothSocket.connect();
+
+            // 데이터 송,수신 스트림을 얻어옵니다.
+            outputStream = bluetoothSocket.getOutputStream();
+            inputStream = bluetoothSocket.getInputStream();
+
+            // 데이터 수신 함수 호출
+            receiveData();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void receiveData() {
+
+        final Handler handler = new Handler();
+        // 데이터를 수신하기 위한 버퍼를 생성
+        readBufferPosition = 0;
+        readBuffer = new byte[1024];
+
+        // 데이터를 수신하기 위한 쓰레드 생성
+        workerThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                while(Thread.currentThread().isInterrupted()) {
+                    try {
+                        // 데이터를 수신했는지 확인합니다.
+                        int byteAvailable = inputStream.available();
+
+                        // 데이터가 수신 된 경우
+                        if(byteAvailable > 0) {
+                            // 입력 스트림에서 바이트 단위로 읽어 옵니다.
+                            byte[] bytes = new byte[byteAvailable];
+                            inputStream.read(bytes);
+
+                            // 입력 스트림 바이트를 한 바이트씩 읽어 옵니다.
+                            for(int i = 0; i < byteAvailable; i++) {
+                                byte tempByte = bytes[i];
+
+                                // 개행문자를 기준으로 받음(한줄)
+                                if(tempByte == '\n') {
+                                    // readBuffer 배열을 encodedBytes로 복사
+                                    byte[] encodedBytes = new byte[readBufferPosition];
+                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
+                                    // 인코딩 된 바이트 배열을 문자열로 변환
+                                    final String text = new String(encodedBytes, "US-ASCII");
+                                    readBufferPosition = 0;
+                                    handler.post(new Runnable() {
+
+                                        @Override
+                                        public void run() {
+                                            if(text == "vol_up"){
+                                                volUp();
+                                            }
+                                            else if(text == "vol_down"){
+                                                volDown();
+                                            }
+                                            else if(text == "next"){
+                                                nextSong();
+                                            }
+                                            else if(text == "prev"){
+                                                prevSong();
+                                            }
+                                        }
+                                    });
+                                } // 개행 문자가 아닐 경우
+                                else {
+                                    readBuffer[readBufferPosition++] = tempByte;
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        // 0.5초마다 받아옴
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        workerThread.start();
+    }
+
     private void update() {
         if (isPlay) {
             play.setBackground(getResources().getDrawable(R.drawable.pause));
         } else {
-            play.setBackground(getResources().getDrawable(R.drawable.play   ));
+            play.setBackground(getResources().getDrawable(R.drawable.play));
         }
 
         title.setText(songTitle[songIdx]);
         thumbnail.setBackground(getResources().getDrawable(songThumbnail[songIdx]));
     }
 
-    private void volUp(){
-        AudioManager am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+    private void volUp() {
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         int volume = am.getStreamVolume(AudioManager.STREAM_MUSIC);
 
-        if(volume > 0) {
-            am.setStreamVolume(AudioManager.STREAM_MUSIC, volume-1, AudioManager.FLAG_PLAY_SOUND);
+        if (volume > 0) {
+            am.setStreamVolume(AudioManager.STREAM_MUSIC, volume - 1, AudioManager.FLAG_PLAY_SOUND);
         }
     }
 
-    private void volDown(){
-        AudioManager am = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+    private void volDown() {
+        AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         int volume = am.getStreamVolume(AudioManager.STREAM_MUSIC);
 
-        if(volume < 15) {
-            am.setStreamVolume(AudioManager.STREAM_MUSIC, volume+1, AudioManager.FLAG_PLAY_SOUND);
+        if (volume < 15) {
+            am.setStreamVolume(AudioManager.STREAM_MUSIC, volume + 1, AudioManager.FLAG_PLAY_SOUND);
         }
+    }
+
+    private void nextSong(){
+        if (songIdx == songID.length - 1) {
+            songIdx = 0;
+        } else {
+            songIdx++;
+        }
+
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+        mediaPlayer = MediaPlayer.create(MainActivity.this, songID[songIdx]);
+        mediaPlayer.start();
+        isPlay = true;
+        update();
+    }
+
+    private void prevSong(){
+        if (songIdx == 0) {
+            songIdx = songID.length - 1;
+        } else {
+            songIdx--;
+        }
+
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+
+        mediaPlayer = MediaPlayer.create(MainActivity.this, songID[songIdx]);
+        mediaPlayer.start();
+        isPlay = true;
+        update();
     }
 
     @Override
