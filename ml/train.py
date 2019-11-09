8dfa82936f8936ddfc0ccae690b11a3277ded8d1
tyg04308 <tyg04308@naver.com>
2019-11-09 23:45:13 +0900

    add auto remove & refresh

--- ml/train.py
+++ ml/train.py
@@ -23,61 +23,64 @@ bus = smbus.SMBus(1)
 Device_Address = 0x68   # MPU6050 device address
 
 def MPU_Init():
-	#write to sample rate register
-	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+    #write to sample rate register
+    bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
 	
-	#Write to power management register
-	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+    #Write to power management register
+    bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
 	
-	#Write to Configuration register
-	bus.write_byte_data(Device_Address, CONFIG, 0)
+    #Write to Configuration register
+    bus.write_byte_data(Device_Address, CONFIG, 0)
 	
-	#Write to Gyro configuration register
-	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+    #Write to Gyro configuration register
+    bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
 	
-	#Write to interrupt enable register
-	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
+    #Write to interrupt enable register
+    bus.write_byte_data(Device_Address, INT_ENABLE, 1)
+    print ("Initalized GPIO!")
+    print ()
+
 
 def read_raw_data(addr):
-	#Accelero and Gyro value are 16-bit
-        high = bus.read_byte_data(Device_Address, addr)
-        low = bus.read_byte_data(Device_Address, addr+1)
+    #Accelero and Gyro value are 16-bit
+    high = bus.read_byte_data(Device_Address, addr)
+    low = bus.read_byte_data(Device_Address, addr+1)
     
-        #concatenate higher and lower value
-        value = ((high << 8) | low)
+    #concatenate higher and lower value
+    value = ((high << 8) | low)
         
-        #to get signed value from mpu6050
-        if(value > 32768):
-                value = value - 65536
-        return value
+    #to get signed value from mpu6050
+    if(value > 32768):
+        value = value - 65536
+    return value
 
 
 def record():
 	
-	#Read Accelerometer raw value
-        acc_x = read_raw_data(ACCEL_XOUT_H)
-        acc_y = read_raw_data(ACCEL_YOUT_H)
-        acc_z = read_raw_data(ACCEL_ZOUT_H)
+    #Read Accelerometer raw value
+    acc_x = read_raw_data(ACCEL_XOUT_H)
+    acc_y = read_raw_data(ACCEL_YOUT_H)
+    acc_z = read_raw_data(ACCEL_ZOUT_H)
 	
-	#Read Gyroscope raw value
-        gyro_x = read_raw_data(GYRO_XOUT_H)
-        gyro_y = read_raw_data(GYRO_YOUT_H)
-        gyro_z = read_raw_data(GYRO_ZOUT_H)
+    #Read Gyroscope raw value
+    gyro_x = read_raw_data(GYRO_XOUT_H)
+    gyro_y = read_raw_data(GYRO_YOUT_H)
+    gyro_z = read_raw_data(GYRO_ZOUT_H)
 	
-	#Full scale range +/- 250 degree/C as per sensitivity scale factor
-        Ax = acc_x/16384.0
-        Ay = acc_y/16384.0
-        Az = acc_z/16384.0
+    #Full scale range +/- 250 degree/C as per sensitivity scale factor
+    Ax = acc_x/16384.0
+    Ay = acc_y/16384.0
+    Az = acc_z/16384.0
 	
-        Gx = gyro_x/131.0
-        Gy = gyro_y/131.0
-        Gz = gyro_z/131.0
+    Gx = gyro_x/131.0
+    Gy = gyro_y/131.0
+    Gz = gyro_z/131.0
 
-        record = [str(round(Gx,4)),  str(round(Gy,4)), str(round(Gz,4)),  str(round(Ax,4)), str(round(Ay,4)), str(round(Az, 4))]
+    record = [str(round(Gx,4)),  str(round(Gy,4)), str(round(Gz,4)),  str(round(Ax,4)), str(round(Ay,4)), str(round(Az, 4))]
 
-        print (record)
-        sleep(0.05)
-        return record
+    print (record)
+    sleep(0.05)
+    return record
 
 def get_motion_name():
     parser = argparse.ArgumentParser()
@@ -109,35 +112,52 @@ def isExistDir(directory):
     
 if __name__ == '__main__':
     MPU_Init()
-    print ("Initalized GPIO!")
-    print ()
     
     motion, max_record = get_motion_name()
     result_path = './dataset/' + motion + '/'
     isExistDir(result_path)
+    isFileOpen = False
 
     cnt = len(os.listdir(result_path))
    
     print ("current data in", str(result_path), ": ", cnt)
 
-    while True:
+    try:
+        while True:
 
-        c = input("input s or else: ")
+            c = input("input 's' or else ('r' to refresh): ")
 
-        if('s' == c):
-            bundle = []
-            total = 0
+            if('s' == c):
+                bundle = []
+                total = 0
 
-            while total < 17:
-                line = record()
-                bundle.append(line)
-                total += 1
+                while total < 17:
+                    line = record()
+                    bundle.append(line)
+                    total += 1
 
-            f_name = result_path + str(cnt) + '.txt'
-            with open(f_name, 'wb') as f:
-                pickle.dump(bundle, f)
+                f_name = result_path + str(cnt) + '.txt'
 
-        else:
-            break
+                isFileOpen = True
+                with open(f_name, 'wb') as f:
+                    pickle.dump(bundle, f)
+                isFileOpen = False
 
-        cnt += 1
+            if('r' == c):
+                cnt = len(os.listdir(result_path))
+                print ("Current data in", str(result_path), ": ", cnt)
+
+            else:
+                break
+
+            cnt += 1
+
+    except:
+        print ()
+        print ("Error!")
+
+    finally:
+        if isFileOpen:
+            cnt = len(os.listdir(result_path))
+            cnt -= 1
+            remove(result_path + str(cnt) + '.txt'
