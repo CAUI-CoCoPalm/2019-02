1ebd1ea43f48500100377c1f6cf2dea59d018ee6
tyg04308 <tyg04308@naver.com>
2020-08-23 22:17:18 +0900

    change path

--- Server_Client/data/csv2txt.py
+++ Server_Client/data/csv2txt.py
@@ -2,14 +2,16 @@ import os
 import pandas as pd
 import pickle
 
-for csv in os.listdir('./dataset'):
+root_dir = '../dataset/'
+
+for csv in os.listdir(root_dir):
     motion = csv.split('.')[0]
 
-    data = pd.read_csv('./dataset/' + csv)
+    data = pd.read_csv(os.path.join(root_dir, csv))
 
     print (f"\nLoad {motion} !\n")
 
-    motion_dir = os.path.join('./dataset', motion)
+    motion_dir = os.path.join(root_dir, motion)
     
     os.mkdir(motion_dir + '/')
 
@@ -17,7 +19,7 @@ for csv in os.listdir('./dataset'):
         line = data.loc[i, :]
         file_name = os.path.join(motion_dir, f'{i:05}.txt') 
         
-        with open(file_name, 'w') as f:
+        with open(file_name, 'wb') as f:
             for j in range(17):
                 col = j*6
                 record = [line[col + 0], line[col+1], line[col+2], line[col+3], line[col+4], line[col+5]]
