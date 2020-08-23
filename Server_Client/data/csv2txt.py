ff0f08d517d97ccc2914439588f6d56bf9ff8220
tyg04308 <tyg04308@naver.com>
2020-08-23 22:15:26 +0900

    change decode.py to csv2txt.py

--- Server_Client/data/csv2txt.py
+++ Server_Client/data/csv2txt.py
@@ -20,7 +20,7 @@ for csv in os.listdir('./dataset'):
         with open(file_name, 'w') as f:
             for j in range(17):
                 col = j*6
-                f.write(f'{line[col + 0]} {line[col+1]} {line[col+2]} {line[col+3]} {line[col+4]} {line[col+5]}')
-
+                record = [line[col + 0], line[col+1], line[col+2], line[col+3], line[col+4], line[col+5]]
+                pickle.dump (record, f)
 
     print ("Finish!")
